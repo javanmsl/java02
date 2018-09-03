b4896bd6030f43de9b46b014eef3d01a8158d371
root <root@itheima.com>
2018-09-03 20:48:10 +0800

    gan

--- git01/src/itcast/First.java
+++ git01/src/itcast/First.java
@@ -2,6 +2,11 @@ package itcast;
 
 public class First {
     public static void main(String[] args) {
+        int i = 10;
         System.out.println ("hello,world");
+        System.out.println (i);
     }
+
+
+
 }
